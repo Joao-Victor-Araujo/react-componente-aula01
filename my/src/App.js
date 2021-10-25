5d47ebe92c8ad431d863fecb1cb4fb01b3ceb218
unknown <jvas1999@gmail.com>
2021-10-25 18:46:11 -0300

    up das atv

---
+++ my/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Message} from './Message';
+
+export const App = () =>{
+  return(
+  <div classsName="App">
+    <h3>test 1234</h3>
+<Message/>
+</div>
+);  
+}
+  
+
---
+++ my/src/Message.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Message = () => (
+    <div>
+        <h1>Hello World React</h1>
+    </div>
+);
+
+
