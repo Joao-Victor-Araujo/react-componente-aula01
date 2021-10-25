95039ac7d6f1a3418e0211ca43460ee266427b70
unknown <jvas1999@gmail.com>
2021-10-25 18:49:31 -0300

    beta envio

--- my/src/Message.js
+++ my/src/Message.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Message = () => (
     <div>
-        <h1>Hello World React</h1>
+        <h1>Otimismo é a chave, motivação a porta, e potencial o caminho para a grande conquista que se chama SONHO.</h1>
     </div>
 );
 
